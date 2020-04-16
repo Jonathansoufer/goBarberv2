218874a3f9040bd5cbc0e32b470763ba8fe3528d
Jonathan Ferreira <jonathanferreira23@gmail.com>
2020-04-16 13:35:41 +0100

    Adding typescript nodeStructure

---
+++ src/routes/sessions.routes.ts
@@ -0,0 +1,22 @@
+import { Router } from 'express';
+
+import CreateSessionService from '../services/CreateSessionService';
+
+const sessionsRoutes = Router();
+
+sessionsRoutes.post('/', async (request, response) => {
+  const { email, password } = request.body;
+
+  const createSessionService = new CreateSessionService();
+
+  const { user, token } = await createSessionService.execute({
+    email,
+    password,
+  });
+
+  delete user.password;
+
+  return response.json({ user, token });
+});
+
+export default sessionsRoutes;
